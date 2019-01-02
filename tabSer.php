6dbf193739fbed004d9a07f7b2a64ec94b5c8e17
rockettravelsystem <46318802+rockettravelsystem@users.noreply.github.com>
2019-01-02 14:07:43 +0200

    Add files via upload

---
+++ tabSer.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name:       tabSer
+ * Description:       tab matters, do it well!
+ * Version:           1.1.0
+ * Author:            RTS team
+ * Text Domain:       tabSer
+ * GitHub Plugin URI: https://github.com/2Fwebd/feedier-wordpress
+ */
